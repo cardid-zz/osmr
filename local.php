7d93be54e9096c791ab4a3d1036d48caf9e7775c
cardid-zz <>
2021-03-26 20:31:46 +0700

    fix

--- local.php
+++ local.php
@@ -4,10 +4,10 @@
  @define('CONST_Postgis_Version', '3');
  // Website settings
  @define('CONST_Website_BaseURL', '/');
- @define('CONST_Replication_Url', getenv("DOWNLOAD_PBF_UPDATES"));
+ @define('CONST_Replication_Url', 'http://download.geofabrik.de/russia/siberian-fed-district-updates');
  @define('CONST_Replication_MaxInterval', '86400');     // Process each update separately, osmosis cannot merge multiple updates
  @define('CONST_Replication_Update_Interval', '86400');  // How often upstream publishes diffs
- @define('CONST_Replication_Recheck_Interval', '900');   // How long to sleep if no update found yet
+ @define('CONST_Replication_Recheck_Interval', '30');   // How long to sleep if no update found yet
  @define('CONST_Pyosmium_Binary', '/usr/local/bin/pyosmium-get-changes');
  //@define('CONST_Database_DSN', 'pgsql:host=192.168.1.128;port=6432;user=nominatim;password=password1234;dbname=nominatim'); <driver>:host=<host>;port=<port>;user=<username>;password=<password>;dbname=<database>
 ?>
